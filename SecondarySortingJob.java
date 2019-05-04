8772f49ff717d4e7d10878e8ce77a34407a4a986
sumitganguly85 <sumitganguly85@gmail.com>
2019-05-04 19:48:55 +0530

    Update SecondarySortingJob.java

--- SecondarySortingJob.java
+++ SecondarySortingJob.java
@@ -2,6 +2,11 @@ package com.mapreduce.secondarysorting;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+//import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.mapreduce.Job;
@@ -44,7 +49,27 @@ public class SecondarySortingJob extends Configured implements Tool{
         Configuration conf = super.getConf();
         Job SecondarySortingJob = Job.getInstance(conf,this.getClass().getName());
 
+        FileInputFormat.setInputPaths(SecondarySortingJob, new Path(args[0]));
+        FileOutputFormat.setOutputPath(SecondarySortingJob, new Path(args[1]));
+
         SecondarySortingJob.setJarByClass(SecondarySortingJob.class);
+
+        SecondarySortingJob.setMapperClass(SecondarySortingMapper.class);
+        SecondarySortingJob.setMapOutputKeyClass(SecondarySortingKeyClass.class);
+        SecondarySortingJob.setMapOutputKeyClass(NullWritable.class);
+
+        SecondarySortingJob.setPartitionerClass(SecondarySortingPartitioner.class);
+
+        SecondarySortingJob.setSortComparatorClass(SecondarySortingKeyComparator.class);
+
+        SecondarySortingJob.setGroupingComparatorClass(SecondarySortingGroupComparator.class);
+
+        SecondarySortingJob.setReducerClass(SecondarySortingReducer.class);
+        SecondarySortingJob.setOutputKeyClass(SecondarySortingKeyClass.class);
+        SecondarySortingJob.setOutputValueClass(NullWritable.class);
+
+        SecondarySortingJob.setNumReduceTasks(4);
+
 
         //SecondarySortingJob.setCom
 
